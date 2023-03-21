6f1649a67185593db7f5c45af9348cd670d0ee4e
Tolu Akinrinola <remmakins@gmail.com>
2023-03-21 03:11:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  function scuberGreetingForFeet(){
+    `gives customer a free sample if the ride is less than or equal to 400 feet`
+  }
 }
+  function scuberGreetingForFeet(){
+    `charges 20 dollars for a distance between 400 and 2000 feet`
+       return "That will be twenty bucks"
+  }
+
+  function scuberGreetingForFeet(distance) {
+    if  (distance > 400 && distance <= 2000) 
+      return 'That will be twenty bucks.';
+  }  
+   
+  
+  
+  function scuberGreetingForFeet(someNumber) {
+
+    if (someNumber <= 400){
+            return "This one is on me!";
+    }
+    else if (someNumber > 2000 && someNumber < 2500){
+        return "I will gladly take your thirty bucks.";
+    }
+    else if(someNumber > 2500){
+        return "No can do.";
+    }
+    else if(someNumber >400 && someNumber <= 2000){
+      return "That will be twenty bucks.";
+
+}
+  
+  }
+    
+  
+  
+
 
 function ternaryCheckCity(){
   // Write your code here!
 }
+function ternaryCheckCity(city){
+  if (city ==='NYC') {
+    return 'Ok, sounds good.';
+  }
+  else if (city !== 'NYC') {
+    return 'No go.';
+  }
+
+}
 
 function switchOnCharmFromTip(){
   // Write your code here!
+} 
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous': 
+    return'Thank you so much.'; 
+    break;
+    case'not as generous':
+    return'Thank you.';
+    break;
+    default:
+      return'Bye.';
+      break;
+  }      
+
 }
